80ad5452abbc095ba8e6fc047d1dff964de81f3d
akaAminZhi <98566204+akaAminZhi@users.noreply.github.com>
2026-04-27 09:52:41 -0400

    Redesign app shell to Apple-style header dropdown navigation

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,12 +1,13 @@
 import Logo from "./Logo";
 import MainNav from "./MainNav";
 
-function Sidebar() {
+function Sidebar({ className = "" }) {
   return (
-    <aside className="flex flex-col gap-3 p-12 row-span-full border-r-2 border-r-gray-100">
+    <aside
+      className={`flex flex-col gap-3 p-12 border-r-2 border-r-gray-100 ${className}`}
+    >
       <Logo></Logo>
       <MainNav></MainNav>
-      
     </aside>
   );
 }
